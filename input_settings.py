26925504b6268994f7faa4976463c180e7f6ff83
Dahsleman <dahsleman2023@gmail.com>
2023-09-27 15:13:07 -0300

    deleted js files

--- input_settings.py
+++ input_settings.py
@@ -18,40 +18,6 @@ class InputSettings:
     
     DIRECTORY_PATH = "G:/My Drive/kompru/data"
 
-    INPUT_SITE = False
-    GENERATE_EXCEL = False
-    WORKBOOK_PATH = './Workbook.xlsx'
-
-    df_name = pd.read_excel(WORKBOOK_PATH, sheet_name='name')
-    df_address = pd.read_excel(WORKBOOK_PATH, sheet_name='address')
-    df_query = pd.read_excel(WORKBOOK_PATH, sheet_name='query')
-    df_spreadsheet_id = pd.read_excel(WORKBOOK_PATH, sheet_name='spreadsheet_id')
-
-    for i in df_name.index:
-        name = df_name.iloc[i]['Name']
-        address = df_address.iloc[i]['Address']
-        term = df_query.iloc[i]['Query']  
-        query_dict = {(term, "kg"): ()}  
-        spreadsheet_id = df_spreadsheet_id.iloc[i]['Spreadsheet_ID']
-
-    SITE = [
-        {
-            "__NAME__": name,
-            "__ADDRESS__": address,
-            "__QUERY__": query_dict, 
-            "__SPREADSHEET_ID__": spreadsheet_id
-        }    
-    ] 
-
-    SEARCH_DETAILS = [
-        {
-            "__ADDRESS__": address,
-            "__TERM__":term,
-            "__SPREADSHEET_ID__":spreadsheet_id,
-            "__PRODUCT_NAME__":'sabonete pielsana liquido antisseptico com phmb 500ml',
-        }
-    ]
-
     directory_path = "./data/s3"
     json_file_name = f'{directory_path}/input'
 
